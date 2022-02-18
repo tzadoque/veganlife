d1ffea086048467ff84349cf5ce925769fef6929
Z4doque <t.zadoque@escolar.ifrn.edu.br>
2022-02-18 13:27:21 -0300

    Adicionando a funcionalidade de login e cadastro, e as páginas 'landing-page.html' e 'dashboard.html'

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from app import create_app
-import os
+# import os
 
 app = create_app()
 
 if __name__ == "__main__":
-  port = int(os.getenv('PORT'), '5000')
-  app.run(host='0.0.0.0', port=port)
+  app.run(debug=True)
+
+# if __name__ == "__main__":
+#   port = int(os.getenv('PORT'), '5000')
+#   app.run(host='0.0.0.0', port=port)
